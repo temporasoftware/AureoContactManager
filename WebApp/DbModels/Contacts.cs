a4764bde4041403b770d9d5acb84c6aeb1d01bcc
Áureo Lopes <47197358+temporasoftware@users.noreply.github.com>
2023-08-05 11:30:57 -0300

    Added the missing DataAnnotations to validate the Contacts class

--- WebApp/DbModels/Contacts.cs
+++ WebApp/DbModels/Contacts.cs
@@ -17,13 +17,14 @@ namespace WebApp.DbModels
         [StringLength(32)]
         public string Id { get; set; }
 
-        [StringLength(60)]
+        [StringLength(60, ErrorMessage = "The Name field cannot have more than 60 characters")]
         public string Name { get; set; }
 
-        [StringLength(9)]
+        [StringLength(9, ErrorMessage = "The Contact field cannot have more than 9 characters")]
         public string Contact { get; set; }
 
-        [StringLength(60)]
+        [StringLength(60, ErrorMessage = "The Contact field cannot have more than 60 characters")]
+        [EmailAddress(ErrorMessage = "The email address is invalid.")]
         public string Email { get; set; }
         
         public bool Audit_RecordStatus { get; set; }
