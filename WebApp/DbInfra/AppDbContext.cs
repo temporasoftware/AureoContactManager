4111b03a7092905e4c86e40e3fb6ed0de5fe2373
Áureo Lopes <47197358+temporasoftware@users.noreply.github.com>
2023-08-05 08:52:03 -0300

    Adding the AppDbContext class to implement the context

---
+++ WebApp/DbInfra/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.DbModels;
+
+namespace WebApp.DbInfra
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Contacts> Contacts { get; set; }
+
+    }
+}
