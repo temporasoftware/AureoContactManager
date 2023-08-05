59f251c597b3aea71e282d27bd59bee96cc56b61
Áureo Lopes <47197358+temporasoftware@users.noreply.github.com>
2023-08-05 12:48:11 -0300

    Layout changes made so that the TABLE fulfills the project requirements

---
+++ WebApp/Helpers/EditPageHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApp.Helpers
+{
+    public class EditPageHelpers
+    {
+
+        public enum EditPageMode
+        {
+            None = 0,
+            View = 1,
+            Edit = 2,
+            AddNew = 3,
+        }
+
+
+    }
+}
