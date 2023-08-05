d599a9cc21e7bee6c4a66b67e2433c0792c45dfb
Áureo Lopes <47197358+temporasoftware@users.noreply.github.com>
2023-08-05 08:32:14 -0300

    Creating the Database Model
    It will be a single table named Contacts

---
+++ WebApp/DbModels/Contacts.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.DbModels
+{
+    public class Contacts
+    {
+
+        public Contacts()
+        {
+            this.Id = Guid.NewGuid().ToString("N");
+        }
+
+
+        [Key]
+        [Column("Id")]
+        [StringLength(32)]
+        public string Id { get; set; }
+
+        [StringLength(60)]
+        public string Name { get; set; }
+
+        [StringLength(9)]
+        public string Contact { get; set; }
+
+        [StringLength(60)]
+        public string Email { get; set; }
+        
+        public bool Audit_RecordStatus { get; set; }
+
+
+    }
+}
